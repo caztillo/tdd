a83257eb0a5dc30ce0a48fee064df52d62bdff3d
Pablo Castillo <caztillo@gmail.com>
2015-10-10 16:59:39 -0500

    include

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url, patterns, include
 
 from lists import views
 
 urlpatterns = patterns('',
                        url(r'^$', 'lists.views.home_page', name='home'),
-                       url(r'^lists/', include('lists.urls'))
+                       url(r'^lists/', include('lists.urls')),
                        )
