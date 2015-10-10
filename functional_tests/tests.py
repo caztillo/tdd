e0ba68945607b390a88e24cddf8a2afac2884231
Pablo Castillo <caztillo@gmail.com>
2015-10-10 14:17:41 -0500

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,4 +88,4 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-        # Satisified, they both go back to sleep
+        # Satisfied, they both go back to sleep
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 
 from lists import views
 
-urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
-    # url(r'^admin/', include(admin.site.urls)),
-]
+urlpatterns = patterns('',
+url(r'^$', 'lists.views.home_page', name='home'),
+url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+                       )
