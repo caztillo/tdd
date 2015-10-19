046335c0ed77089e87a5fd96ee97bdfe3f253047
Mayanin Sobrevilla <mayanin.sobrevilla@gmail.com>
2015-10-19 13:26:31 -0500

    using_the_existing_list_item_form_in_the_list_view

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -2,11 +2,13 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 
-class ListAndItemModelsTest(TestCase):
+
+class ItemModelTest(TestCase):
 
     def test_default_text(self):
         item = Item()
         self.assertEqual(item.text, '')
+
 
     def test_item_is_related_to_list(self):
         list_ = List.objects.create()
@@ -14,6 +16,7 @@ class ListAndItemModelsTest(TestCase):
         item.list = list_
         item.save()
         self.assertIn(item, list_.item_set.all())
+
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -23,10 +26,6 @@ class ListAndItemModelsTest(TestCase):
             item.full_clean()
 
 
-    def test_get_absolute_url(self):
-        list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
-
 
     def test_duplicate_items_are_invalid(self):
         list_ = List.objects.create()
@@ -35,12 +34,14 @@ class ListAndItemModelsTest(TestCase):
             item = Item(list=list_, text='bla')
             item.full_clean()
 
+
     def test_CAN_save_same_item_to_different_lists(self):
         list1 = List.objects.create()
         list2 = List.objects.create()
         Item.objects.create(list=list1, text='bla')
         item = Item(list=list2, text='bla')
-        item.full_clean() # should not raise
+        item.full_clean()  # should not raise
+
 
     def test_list_ordering(self):
         list1 = List.objects.create()
@@ -52,6 +53,16 @@ class ListAndItemModelsTest(TestCase):
             [item1, item2, item3]
         )
 
+
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
+
+
+class ListModelTest(TestCase):
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
+
